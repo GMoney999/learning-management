fa94826eb1511762e0e734d351fa88fc729f926e
Gerami Sadeghi <Gerami.Sadeghi@gmail.com>
2024-12-08 14:04:40 -0800

    fixed environmental variable typo

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import Course from "../types/index"; 
+
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
   tagTypes: ["Courses"],
   endpoints: (build) => ({
-    getCourses: build.query<Course[], { category?: string }>({
+    getCourses: build.query<Course, { category?: string }>({
       query: ({ category }) => ({
         url: "courses",
         params: { category },
