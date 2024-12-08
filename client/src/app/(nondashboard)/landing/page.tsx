e927dca47eec62cc2fe2a56d8c7c79e68487a4f6
Gerami Sadeghi <Gerami.Sadeghi@gmail.com>
2024-12-08 15:38:03 -0800

    added course card search component

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -7,6 +7,8 @@ import Image from 'next/image'
 import { useCarousel } from '@/hooks/useCarousel'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useGetCoursesQuery } from '@/state/api'
+import CourseCardSearch from '@/components/CourseCardSearch'
+import { useRouter } from 'next/navigation'
 
 const LoadingSkeleton = () => {
     return (
@@ -40,83 +42,104 @@ const LoadingSkeleton = () => {
 }
 
 const Landing = () => {
- const currentImage = useCarousel({ totalImages: 3});
+    const router = useRouter();
+    const currentImage = useCarousel({ totalImages: 3});
 
- const { data: courses, isLoading, isError } = useGetCoursesQuery({});
- console.log("courses: ", courses);
+    const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+
+    const handleCourseClick = (courseId: string) => {
+        router.push(`/search?id=${courseId}`)
+    }
+
+    console.log("courses: ", courses);
 
 
-
-
-  return (
-    <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 0.5}}
-        className='landing'
-    >
+    return (
         <motion.div
-            initial={{y: 20, opacity: 0}}
-            animate={{y: 0, opacity: 1}}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
             transition={{duration: 0.5}}
-            className='landing__hero'
+            className='landing'
         >
-            <div className='landing__hero-content'>
-                <h1 className='landing__title'>Courses</h1>
-                <p className='landing__description'>
-                    This is the list of courses that you can enroll in.
-                    <br/>
-                    Courses when you need them and want them.
-                </p>
-                <div className='landing__cta'>
-                    <Link href='/search'>
-                        <div className='landing__cta-button'>Search for courses</div>
-                    </Link>
+            <motion.div
+                initial={{y: 20, opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                transition={{duration: 0.5}}
+                className='landing__hero'
+            >
+                <div className='landing__hero-content'>
+                    <h1 className='landing__title'>Courses</h1>
+                    <p className='landing__description'>
+                        This is the list of courses that you can enroll in.
+                        <br/>
+                        Courses when you need them and want them.
+                    </p>
+                    <div className='landing__cta'>
+                        <Link href='/search'>
+                            <div className='landing__cta-button'>Search for courses</div>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <div className='landing__hero-images'>
-                {["/hero1.jpg", "/hero2.jpg", "/hero3.jpg"].map((src, index) => (
-                    <Image
-                        key={src}
-                        src={src}
-                        alt={`Hero Banner ${index + 1}`}
-                        fill 
-                        priority={index === currentImage}
-                        sizes='(max-with: 760) 100vw, (max-width: 1200) 50vw, 33vw'
-                        className={`landing__hero-image ${
-                            index === currentImage ? "landing__hero-image--active" : ""
-                        }`}
-                    />
-                ))}
-            </div>
-        </motion.div>
-        <motion.div
-            initial={{y: 20, opacity: 0}}
-            whileInView={{y: 0, opacity: 1}}
-            transition={{duration: 0.5}}
-            viewport={{amount: 0.3, once: true}}
-            className='landing__featured'
-        >
-            <h2 className='landing__featured-title'>Featured Courses</h2>
-            <p className='landing__featured-description'>From beginning to advanced, in all industries, we have just the right courses for you!</p>
+                <div className='landing__hero-images'>
+                    {["/hero1.jpg", "/hero2.jpg", "/hero3.jpg"].map((src, index) => (
+                        <Image
+                            key={src}
+                            src={src}
+                            alt={`Hero Banner ${index + 1}`}
+                            fill 
+                            priority={index === currentImage}
+                            sizes='(max-with: 760) 100vw, (max-width: 1200) 50vw, 33vw'
+                            className={`landing__hero-image ${
+                                index === currentImage ? "landing__hero-image--active" : ""
+                            }`}
+                        />
+                    ))}
+                </div>
+            </motion.div>
+            <motion.div
+                initial={{y: 20, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.5}}
+                viewport={{amount: 0.3, once: true}}
+                className='landing__featured'
+            >
+                <h2 className='landing__featured-title'>Featured Courses</h2>
+                <p className='landing__featured-description'>From beginning to advanced, in all industries, we have just the right courses for you!</p>
 
-            <div className='landing__tags'>
-                {[
-                    "web development",
-                    "enterprise IT",
-                    "react nextjs",
-                    "javascript",
-                    "backend development"
-                ].map((tag, index) => (
-                    <span key={index} className='landing__tag'>{tag}</span>
-                ))}
-            </div>
-            <div className="landing__courses">
-                {/* COURSES DISPLAY */}
-            </div>
+                <div className='landing__tags'>
+                    {[
+                        "web development",
+                        "enterprise IT",
+                        "react nextjs",
+                        "javascript",
+                        "backend development"
+                    ].map((tag, index) => (
+                        <span key={index} className='landing__tag'>{tag}</span>
+                    ))}
+                </div>
+                <div className="landing__courses">
+                    { courses && 
+                    courses
+                        .slice(0, 4)
+                        .map((course, index) => (
+                            <motion.div
+                            key={course.courseId}
+                            initial={{y: 50, opacity: 0}}
+                            whileInView={{y: 0, opacity: 1}}
+                            transition={{duration: 0.5, delay: index * 0.2}}
+                            viewport={{amount: 0.3 }}
+                            >
+                            <CourseCardSearch 
+                                course={course} 
+                                onClick={() => handleCourseClick(course.courseId)}
+                                />    
+                            </motion.div>
+                        ))
+                    }
+                </div>
+            </motion.div>
         </motion.div>
-    </motion.div>
-  )
+    )
 }
 
 export default Landing
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,64 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { FetchArgs, BaseQueryApi } from "@reduxjs/toolkit/query";
 
+// Create a baseQuery using fetchBaseQuery from Redux Toolkit. 
+const customBaseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: any
+) => {
+  // configure with a baseUrl taken from an environment variable
+  // typically set to backend server's URL
+  const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  });
 
+  // Attempt to execute base query
+  try {
+    const result: any = await baseQuery(args, api, extraOptions);
+    
+    // If it works (result.data exists), extract nested field "data"
+    // Useful for not having to write courses.data or course.data every time
+    if (result.data) {
+      result.data = result.data.data;
+    }
+
+    return result;
+
+    // If it fails (result.error exists), return the error
+    // Useful for not having to write courses.error or course.error every time
+  } catch (error: unknown) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+
+    return { error: { status: "FETCH_ERROR", data: errorMessage } };
+  }
+}
+
+//// API CONFIGURATION
+// baseQuery:
+//    used as the base query for all endpoints defined in the createApi instance. 
+//    This means every request made through this API will use the logic defined in customBaseQuery 
+//    for making HTTP requests and handling responses or errors.
+//
+// reducerPath:
+//    a string that specifies the key in the Redux store where the API slice's state will be stored, 
+//    like a namespace for the API data in your Redux state.
+// 
+// tagTypes:
+//    array of strings that defines the type of tags that can be used for caching and invalidation.
+//    Tags help manage cache updates when data changes, which allows ui to reflect the latest data.
+//
+// endpoints:
+//    defines api operations we want to perform.
+//    Each endpoint specifies how to construct the request (url, parameters)
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  baseQuery: customBaseQuery,
   reducerPath: "api",
   tagTypes: ["Courses"],
+  // 'build' arg is an object that provides methods for defining api endpoints,
+  // which allows us to define queries and mutations for api
   endpoints: (build) => ({
-    getCourses: build.query<Course, { category?: string }>({
+    getCourses: build.query<Course[], { category?: string }>({
       query: ({ category }) => ({
         url: "courses",
         params: { category },
