0962eeba4cf0cdcf5517db232a19ca5c5dfb2822
Gerami Sadeghi <Gerami.Sadeghi@gmail.com>
2024-12-08 18:38:12 -0800

    Added accordion component for search page

--- client/src/app/(nondashboard)/search/page.tsx
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -6,6 +6,8 @@ import { motion  } from 'framer-motion'
 
 import { useGetCoursesQuery } from '@/state/api'
 import Loading from '@/components/Loading';
+import CourseCardSearch from '@/components/CourseCardSearch'
+import SelectedCourse from './SelectedCourse'
 
 
 const Search = () => {
@@ -39,16 +41,56 @@ const Search = () => {
     if (isLoading) return <Loading />;
     if (isError || !courses) return <div>Failed to fetch courses</div>;
 
+    // Handlers
+    const handleCourseSelect = (course: Course) => {
+        setSelectedCourse(course);
+        router.push(`/search?id=${course.courseId}`); // state stored in URL
+    }
 
+    const handleEnrollNow = (course: Course) => {
+        router.push(`/checkout?step=1&id=${course.courseId}&showSignUp=false`) // state stored in URL
+    }
+
+    
     return (
         <motion.div
-          initial={{opacity: 0}}
+          initial={{opacity: 0}} 
           animate={{opacity: 1}}
           transition={{duration: 0.5}}
           className="search"
         >
             <h1 className="search__title">List of available courses</h1>
             <h2 className="search__subtitle">{courses.length} courses available</h2>
+            <div className="search__content">
+            <motion.div
+                initial={{y: 40, opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                transition={{duration: 0.5, delay: 0.2}}
+                className="search__courses-grid"
+                >
+                    {courses.map((course) => (
+                        <CourseCardSearch 
+                          key={course.courseId}
+                            course={course}
+                            isSelected={selectedCourse?.courseId === course.courseId}
+                            onClick={() => handleCourseSelect(course)}
+                        />
+                    ))}
+                </motion.div>
+                { selectedCourse && (
+                    <motion.div
+                        initial={{y: 40, opacity: 0}}
+                        animate={{y: 0, opacity: 1}}
+                        transition={{duration: 0.5, delay: 0.5}}
+                        className="search__selected-course "
+                    >
+                        <SelectedCourse
+                            course={selectedCourse}
+                            handleEnrollNow={handleEnrollNow}
+                        />
+                    </motion.div>
+                )}
+            </div>
         </motion.div>
     )
 }
